246093ade25a756b2f1a2429d8be00ad3cc0d1b1
Jourdan Rodrigues <thiagojourdan@gmail.com>
2016-12-26 08:47:06 -0300

    Update error codes.

--- errorCodes.js
+++ errorCodes.js
@@ -13,9 +13,11 @@ function getStructure(statusCode, message) {
 
 module.exports = {
     "cielo" : {
+        // http://developercielo.github.io/Webservice-3.0/english.html#error-codes
         114: getStructure(status.HTTP_500_INTERNAL_SERVER_ERROR, "O ID do vendedor está em formato incorreto."),
         119: getStructure(status.HTTP_400_BAD_REQUEST, "Necessária pelo menos 1 forma de pagamento."),
         122: getStructure(status.HTTP_400_BAD_REQUEST, "\"MerchantOrderId\" é obrigatório."),
+        130: getStructure(status.HTTP_400_BAD_REQUEST, "Cartão de crédito não encontrado."),
         184: getStructure(status.HTTP_400_BAD_REQUEST, "Requisição não pode estar vazia.")
     }
 };
