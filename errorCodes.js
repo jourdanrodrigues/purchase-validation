42280c06b945119187fafaf9fe6e81ce38fee023
Jourdan Rodrigues <thiagojourdan@gmail.com>
2016-12-26 10:44:32 -0300

    Improve structure for "errorCodes.js" and create "successCodes.js".

--- errorCodes.js
+++ errorCodes.js
@@ -2,22 +2,21 @@
 
 let status = require("./httpStatus");
 
-function getStructure(statusCode, message) {
-    return {
-        code: statusCode,
-        data: {
-            detail: message
-        }
+let mountData = (apiCode, httpStatus, message) => ({
+    httpStatus: httpStatus,
+    data: {
+        code: apiCode,
+        detail: message
     }
-}
+});
 
 module.exports = {
-    "cielo" : {
+    "cielo": {
         // http://developercielo.github.io/Webservice-3.0/english.html#error-codes
-        114: getStructure(status.HTTP_500_INTERNAL_SERVER_ERROR, "O ID do vendedor está em formato incorreto."),
-        119: getStructure(status.HTTP_400_BAD_REQUEST, "Necessária pelo menos 1 forma de pagamento."),
-        122: getStructure(status.HTTP_400_BAD_REQUEST, "\"MerchantOrderId\" é obrigatório."),
-        130: getStructure(status.HTTP_400_BAD_REQUEST, "Cartão de crédito não encontrado."),
-        184: getStructure(status.HTTP_400_BAD_REQUEST, "Requisição não pode estar vazia.")
+        114: mountData(114, status.HTTP_500_INTERNAL_SERVER_ERROR, "O ID do vendedor está em formato incorreto."),
+        119: mountData(119, status.HTTP_400_BAD_REQUEST, "Necessária pelo menos 1 forma de pagamento."),
+        122: mountData(122, status.HTTP_400_BAD_REQUEST, "\"MerchantOrderId\" é obrigatório."),
+        130: mountData(130, status.HTTP_400_BAD_REQUEST, "Cartão de crédito não encontrado."),
+        184: mountData(184, status.HTTP_400_BAD_REQUEST, "Requisição não pode estar vazia.")
     }
 };
---
+++ successCodes.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let status = require("./httpStatus");
+
+let successData = {
+    "cielo": {
+        // http://developercielo.github.io/Webservice-3.0/english.html#return-codes
+        57: {httpStatus: status.HTTP_400_BAD_REQUEST, message: "Transação não autorizada para este cartão."}
+    }
+};
+
+module.exports = (service, responseData) => {
+    let data = successData[service][responseData.Payment.ReturnCode];
+    return {
+        httpStatus: data.httpStatus,
+        data: {
+            data: responseData,
+            detail: data.message
+        }
+    }
+};
